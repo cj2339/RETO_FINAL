4d1e427ed5c4286d44cab2cf816ecb0cfa98af99
Etna2886 <etna2886gmail.com>
2026-03-24 09:15:12 +0100

    corrección en el main

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -31,46 +31,46 @@ public class MainWindow extends JDialog implements ActionListener {
 	private JButton btnAdmins;
 	private JPanel contentPane;
 	private Image imagenFondo = new ImageIcon("images/ManagementAnchor.png").getImage();	
-	
+
 	//preguntar sobre esta herencia
 	public MainWindow(JDialog father, LoginController cont) {
 		super(father,true);
 		this.cont=cont;
-		
-		 contentPane = new JPanel() {
-		        @Override
-		        protected void paintComponent(Graphics g) {
-		            super.paintComponent(g);
-		            g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
-		        }
-		  };
-		  setContentPane(contentPane);
-		  contentPane.setLayout(null);
-		    
+
+		contentPane = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(imagenFondo, 0, 0, getWidth(), getHeight(), this);
+			}
+		};
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
 		setTitle("Main Management");
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("MANAGEMENT");
 		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 20));
 		lblNewLabel.setBounds(148, 22, 160, 21);
 		getContentPane().add(lblNewLabel);
-		
+
 		btnCruise = new JButton("CRUISE");
 		btnCruise.setBounds(24, 77, 112, 38);
 		getContentPane().add(btnCruise);
 		btnCruise.addActionListener(this);
-		
+
 		btnWorker = new JButton("WORKER");
 		btnWorker.setBounds(299, 77, 112, 38);
 		getContentPane().add(btnWorker);
 		btnWorker.addActionListener(this);
-		
+
 		btnClient = new JButton("CLIENT");
 		btnClient.setBounds(24, 164, 112, 38);
 		getContentPane().add(btnClient);
 		btnClient.addActionListener(this);
-		
+
 		btnAdmins = new JButton("ADMIN");
 		btnAdmins.setBounds(299, 164, 112, 38);
 		getContentPane().add(btnAdmins);
@@ -79,40 +79,38 @@ public class MainWindow extends JDialog implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		
+
+
 		String[] options = {"Add", "Eliminate", "Modify", "See"};
-	    int option;
-	    String type = "";
+		int option;
+		String type = "";
 
-	    if (e.getSource().equals(btnCruise)) {
-	    	type = "cruise";
-	    }else if (e.getSource().equals(btnWorker)) {
-	    	type = "worker";
-	    }else if (e.getSource().equals(btnClient)) {
-	    	type = "client";
-	    }else if (e.getSource().equals(btnAdmins)) {
-	    	type = "book";
-	    }
+		if (e.getSource().equals(btnCruise)) {
+			type = "cruise";
+		}else if (e.getSource().equals(btnWorker)) {
+			type = "worker";
+		}else if (e.getSource().equals(btnClient)) {
+			type = "client";
 
-	    option = JOptionPane.showOptionDialog(this, "Select an option: ", type, JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
+			option = JOptionPane.showOptionDialog(this, "Select an option: ", type, JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, null);
 
-	    switch (option) {
-	        case 0:
-	        	AddWindow add = new AddWindow(this,true,cont,type);
-	        	add.setVisible(true);
-	            break;
-	        case 1:
-	            new DeleteWindow(type).setVisible(true);
-	            break;
-//	        case 2:
-//	            new ModifyWindow(type).setVisible(true);
-//	            break;
-//	        case 3:
-//	            new ViewWindow(type).setVisible(true);
-//	            break;
-	    }
+			switch (option) {
+			case 0:
+				AddWindow add = new AddWindow(this,true,cont,type);
+				add.setVisible(true);
+				break;
+			case 1:
+				new DeleteWindow(type).setVisible(true);
+				break;
+				//	        case 2:
+					//	            new ModifyWindow(type).setVisible(true);
+				//	            break;
+				//	        case 3:
+				//	            new ViewWindow(type).setVisible(true);
+				//	            break;
+			}
 
-	    
+
+		}
 	}
 }
