661d79b5b39337320a87347499bb6f2206509060
cj2339 <ikercarrillo030206@gmail.com>
2026-04-14 13:05:42 +0200

    cambio mínimo

--- src/view/ChangePasswordAdminWindow.java
+++ src/view/ChangePasswordAdminWindow.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
-import javax.swing.JFrame;
 
 import java.awt.Toolkit;
 import javax.swing.JPanel;
