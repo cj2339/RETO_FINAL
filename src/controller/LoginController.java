dde528101f516fdaeec57d5d95f073c8285ec265
Aritzz31 <aritznavatavo@gmail.com>
2026-04-14 16:57:04 +0200

    Comentarioss Loginn

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -17,85 +17,216 @@ import view.StartWindow;
 import model.Client;
 
 public class LoginController {
-	
-	AdministratorDAO daoAdministrator = new DBImplementationAdministrator();// implementation of the AdministratorDAO interface
-	CruiseDAO daoCruise = new DBImplementationCruise();//implementation of the CruiseDAO interface
-	WorkerDAO daoWorker = new DBImplementationWorker();//implementation of the WorkerDAO interface
-	ClientDAO daoClient = new DBImplementationClient();//implementation of the ClientDAO interface
+
+	AdministratorDAO daoAdministrator = new DBImplementationAdministrator();
+	CruiseDAO daoCruise = new DBImplementationCruise();
+	WorkerDAO daoWorker = new DBImplementationWorker();
+	ClientDAO daoClient = new DBImplementationClient();
 	private String loggedInAdminName;
 
 	// Cruise CRUD Methods
-	public List<Cruise> getAllCruise() {//returns a list of all cruises by calling the getAllCruise method of the CruiseDAO implementation
+	/**
+	 * Retrieves a list of all cruises by calling the getAllCruise method of the
+	 * CruiseDAO implementation and returns it.
+	 * 
+	 * @return
+	 */
+	public List<Cruise> getAllCruise() {
 		return daoCruise.getAllCruise();
 	}
 
-	public boolean deleteCruise(String id) {//deletes a cruise by calling the deleteCruise method of the CruiseDAO implementation with the provided id
+	/**
+	 * Deletes a cruise by calling the deleteCruise method of the CruiseDAO
+	 * implementation with the provided id and returns the result.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public boolean deleteCruise(String id) {
 		return daoCruise.deleteCruise(id);
 	}
 
-	public boolean updateCruiseByCode(Cruise cruise) {//updates a cruise by calling the updateCruiseByCode method of the CruiseDAO implementation with the provided cruise object
+	/**
+	 * Updates a cruise by calling the updateCruiseByCode method of the CruiseDAO
+	 * implementation with the provided cruise object and returns the result.
+	 * 
+	 * @param cruise
+	 * @return
+	 */
+	public boolean updateCruiseByCode(Cruise cruise) {
 		return daoCruise.updateCruiseByCode(cruise);
 	}
 
-	public Cruise getCruiseByCode(String id) {//retrieves a cruise by calling the getCruiseByCode method of the CruiseDAO implementation with the provided id
+	/**
+	 * Retrieves a cruise by calling the getCruiseByCode method of the CruiseDAO
+	 * implementation with the provided id and returns it.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Cruise getCruiseByCode(String id) {
 		return daoCruise.getCruiseByCode(id);
 	}
 
-	public boolean insertCruise(Cruise cruise) {//inserts a new cruise by calling the insertCruise method of the CruiseDAO implementation with the provided cruise object
+	/**
+	 * Inserts a new cruise by calling the insertCruise method of the CruiseDAO
+	 * implementation with the provided cruise object and returns the result.
+	 * 
+	 * @param cruise
+	 * @return
+	 */
+	public boolean insertCruise(Cruise cruise) {
 		return daoCruise.insertCruise(cruise);
 	}
 
-	public boolean checkCruiseInWorker(String id) {//checks if a cruise is associated with any worker by calling the checkCruiseInWorker method of the CruiseDAO implementation with the provided id
+	/**
+	 * Checks if a cruise is associated with any worker by calling the
+	 * checkCruiseInWorker method of the CruiseDAO implementation with the provided
+	 * id and returns the result.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public boolean checkCruiseInWorker(String id) {
 		return daoCruise.checkCruiseInWorker(id);
 	}
 
-	public boolean checkCruiseInBook(String id) {//checks if a cruise is associated with any booking by calling the checkCruiseInBook method of the CruiseDAO implementation with the provided id
+	/**
+	 * Checks if a cruise is associated with any booking by calling the
+	 * checkCruiseInBook method of the CruiseDAO implementation with the provided id
+	 * and returns the result.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public boolean checkCruiseInBook(String id) {
 		return daoCruise.checkCruiseInBook(id);
 	}
 
 	// Administrator CRUD Methods
-	public List<Administrator> getAllAdministrators() {//returns a list of all administrators by calling the getAllAdministrators method of the AdministratorDAO implementation
+	/**
+	 * Retrieves a list of all administrators by calling the getAllAdministrators
+	 * method of the AdministratorDAO implementation and returns it.
+	 * 
+	 * @return
+	 */
+	public List<Administrator> getAllAdministrators() {
 		return daoAdministrator.getAllAdministrators();
 	}
 
-	public boolean insertAdministrator(Administrator admin) {//inserts a new administrator by calling the insertUser method of the AdministratorDAO implementation with the provided administrator object
+	/**
+	 * Inserts a new administrator by calling the insertUser method of the
+	 * AdministratorDAO implementation with the provided administrator object and
+	 * returns the result.
+	 * 
+	 * @param admin
+	 * @return
+	 */
+	public boolean insertAdministrator(Administrator admin) {
 		return daoAdministrator.insertUser(admin);
 	}
 
-	public boolean updateAdministrator(Administrator admin) {//updates an administrator by calling the updateAdministrator method of the AdministratorDAO implementation with the provided administrator object
+	/**
+	 * Updates an administrator by calling the updateAdministrator method of the
+	 * AdministratorDAO implementation with the provided administrator object and
+	 * returns the result.
+	 * 
+	 * @param admin
+	 * @return
+	 */
+	public boolean updateAdministrator(Administrator admin) {
 		return daoAdministrator.updateAdministrator(admin);
 	}
 
-	public boolean deleteAdministrator(String name) {//deletes an administrator by calling the deleteAdministrator method of the AdministratorDAO implementation with the provided name
+	/**
+	 * Deletes an administrator by calling the deleteAdministrator method of the
+	 * AdministratorDAO implementation with the provided name and returns the
+	 * result.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public boolean deleteAdministrator(String name) {
 		return daoAdministrator.deleteAdministrator(name);
 	}
 
-	public boolean checkAdminExists(String name) {//checks if an administrator exists by calling the checkAdminExists method of the AdministratorDAO implementation with the provided name
+	/**
+	 * Checks if an administrator exists by calling the checkAdminExists method of
+	 * the AdministratorDAO implementation with the provided name and returns the
+	 * result.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public boolean checkAdminExists(String name) {
 		return daoAdministrator.checkAdminExists(name);
 	}
 
 	// Client CRUD Methods
-	public List<Client> getAllClient() {//returns a list of all clients by calling the getAllClient method of the ClientDAO implementation
+
+	/**
+	 * Retrieves a list of all clients by calling the getAllClient method of the
+	 * ClientDAO implementation and returns it.
+	 * 
+	 * @return
+	 */
+	public List<Client> getAllClient() {
 		return daoClient.getAllClient();
 	}
 
-	public boolean deleteClient(Client client) {//deletes a client by calling the deleteClient method of the ClientDAO implementation with the provided client object
+	/**
+	 * Deletes a client by calling the deleteClient method of the ClientDAO
+	 * implementation with the provided client object and returns the result.
+	 * 
+	 * @param client
+	 * @return
+	 */
+	public boolean deleteClient(Client client) {
 		return daoClient.deleteClient(client);
 	}
 
-	public Client getClientByCode(Client client) {//retrieves a client by calling the getClientByCode method of the ClientDAO implementation with the provided client object
+	/**
+	 * Retrieves a client by calling the getClientByCode method of the ClientDAO
+	 * implementation with the provided client object and returns it.
+	 * 
+	 * @param client
+	 * @return
+	 */
+	public Client getClientByCode(Client client) {
 		return daoClient.getClientByCode(client);
 	}
 
-	public boolean updateClientByCode(Client client) {//updates a client by calling the updateClientByCode method of the ClientDAO implementation with the provided client object
+	/**
+	 * Updates a client by calling the updateClientByCode method of the ClientDAO
+	 * implementation with the provided client object and returns the result.
+	 * 
+	 * @param client
+	 * @return
+	 */
+	public boolean updateClientByCode(Client client) {
 		return daoClient.updateClientByCode(client);
 	}
 
-	public boolean insertClient(Client client) {//inserts a new client by calling the insertClient method of the ClientDAO implementation with the provided client object
+	/**
+	 * Inserts a new client by calling the insertClient method of the ClientDAO
+	 * implementation with the provided client object and returns the result.
+	 * 
+	 * @param client
+	 * @return
+	 */
+	public boolean insertClient(Client client) {
 		return daoClient.insertClient(client);
 	}
 
-	public boolean checkClientInBook(String id) {//checks if a client is associated with any booking by calling the checkClientInBook method of the ClientDAO implementation with the provided id
+	/**
+	 * Checks if a client is associated with any booking by calling the
+	 * checkClientInBook method of the ClientDAO implementation with the provided id
+	 * and returns the result.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public boolean checkClientInBook(String id) {
 		return daoClient.checkClientInBook(id);
 	}
 
@@ -104,11 +235,24 @@ public class LoginController {
 //		return daoWorker.getWorkerCodes();
 //	}
 
-	public List<Worker> getAllWorker() {//returns a list of all workers by calling the getAllWorker method of the WorkerDAO implementation
+	/**
+	 * Retrieves a list of all workers by calling the getAllWorker method of the
+	 * WorkerDAO implementation and returns it.
+	 * 
+	 * @return
+	 */
+	public List<Worker> getAllWorker() {
 		return daoWorker.getAllWorker();
 	}
 
-	public boolean checkUser(Administrator user) {//checks if a user exists by calling the checkUser method of the AdministratorDAO implementation with the provided user object
+	/**
+	 * Inserts a new worker by calling the insertWorker method of the WorkerDAO
+	 * implementation with the provided worker object and returns the result.
+	 * 
+	 * @param worker
+	 * @return
+	 */
+	public boolean checkUser(Administrator user) {
 		return daoAdministrator.checkUser(user);
 	}
 //
@@ -140,20 +284,46 @@ public class LoginController {
 //		return daoWorker.deleteBook(codCruise, idClient);
 //	}
 
-	public String getLoggedInAdminName() {//returns the name of the currently logged-in administrator
+	/**
+	 * Retrieves a worker by calling the getWorkerByCode method of the WorkerDAO
+	 * implementation with the provided id and returns it.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public String getLoggedInAdminName() {
 		return loggedInAdminName;
 	}
 
-	public void setLoggedInAdminName(String loggedInAdminName) {//sets the name of the currently logged-in administrator
+	/**
+	 * Sets the name of the logged-in administrator by assigning the provided name
+	 * to the loggedInAdminName variable.
+	 * 
+	 * @param loggedInAdminName
+	 */
+	public void setLoggedInAdminName(String loggedInAdminName) {
 		this.loggedInAdminName = loggedInAdminName;
 	}
 
-	public boolean updatePassword(String adminName, String newPassword) {//updates the password of an administrator by calling the updatePassword method of the AdministratorDAO implementation with the provided administrator name and new password
+	/**
+	 * Updates the password of an administrator by calling the updatePassword method
+	 * of the AdministratorDAO implementation with the provided administrator name
+	 * and new password and returns the result.
+	 * 
+	 * @param adminName
+	 * @param newPassword
+	 * @return
+	 */
+	public boolean updatePassword(String adminName, String newPassword) {
 		return daoAdministrator.updatePassword(adminName, newPassword);
 	}
 
 	// Windows Methods
-	public void viewWindow() {//creates and displays the start window by instantiating a new StartWindow object with the current LoginController instance and setting it to visible
+	/**
+	 * Creates a new instance of the StartWindow class, passing the current instance
+	 * of LoginController as a parameter, and sets the window to be visible.
+	 */
+	public void viewWindow() {
 		StartWindow win = new StartWindow(this);
 		win.setVisible(true);
 	}
