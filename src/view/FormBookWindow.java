cf29f814ff4ea33ab75a2486433c841a161b7e30
cj2339 <ikercarrillo030206@gmail.com>
2026-04-16 13:43:29 +0200

    arreglo el merch

--- src/view/FormBookWindow.java
+++ src/view/FormBookWindow.java
@@ -290,7 +290,6 @@ public class FormBookWindow extends JDialog implements ActionListener {
 			}
 
 			JOptionPane.showMessageDialog(this, msg, title, type);
-
 			if (success) {
 				parent.refreshModel();
 				this.dispose();
